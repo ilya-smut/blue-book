1207b31b123e5e631b1bb1dec4d69633ad85a8fb
Ilya Smut <p2688910@my365.dmu.ac.uk>
2025-05-12 21:39:17 +0100

    database models to save and retrieve questions

--- bluebook/database_manager.py
+++ bluebook/database_manager.py
@@ -1,5 +1,6 @@
 from sqlmodel import Field, SQLModel, Session, UniqueConstraint, create_engine, select, delete
 from bluebook.confguration import Configuration
+from bluebook import data_models
 
 
 # Data Models
@@ -10,6 +11,20 @@ class ExtraRequest(SQLModel, table=True):
 
     def to_dict(self):
         return {'id': self.id, 'request': self.request}
+    
+class Questions(SQLModel, table=True):
+    __table_args__ = (UniqueConstraint("question"),)
+    id: int | None = Field(default=None, primary_key=True)
+    question: str
+    study_recommendation: str
+
+
+class Choices(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    option: str
+    explanation: str
+    is_correct: bool
+    question_id: int = Field(default=None, foreign_key="questions.id")
 
 
 class Database:
@@ -55,4 +70,56 @@ class Database:
         with Session(self.engine) as session:
             session.exec(delete(ExtraRequest).where(ExtraRequest.request==request))
             session.commit()
+
+
+    def add_question(self, question: data_models.Question):
+        with Session(self.engine) as session:
+            question_to_insert = Questions(question=question.question, study_recommendation=question.study_recommendation)
+            choices_to_map = list[Choices]()
+            for choice in question.choices:
+                choice_to_insert = Choices(
+                    option=choice.option, 
+                    explanation=choice.explanation, 
+                    is_correct=choice.is_correct, 
+                    question_id=question_to_insert.id
+                    )
+                choices_to_map.append(choice_to_insert)
+            session.add(question_to_insert)
+            session.add_all(choices_to_map)
+            session.commit()
+    
+
+    def remove_question_by_id(self, question_id: int):
+        with Session(self.engine) as session:
+            if question:= session.exec(select(Questions).where(Questions.id == question_id)).first():
+                # Question found
+                session.exec(delete(Choices).where(Choices.question_id == question.id))
+                session.exec(delete(Questions).where(Questions.id == question.id))
+                session.commit()
+            else:
+                # Question not found
+                pass
+    
+
+    def select_all_questions_pydantic(self):
+        with Session(self.engine) as session:
+            pydantic_questions = list[data_models.Question]()
+            all_rows = session.exec(select(Questions))
+            for question_row in all_rows:
+                choices_rows = session.exec(select(Choices).where(Choices.question_id == question_row.id))
+                choices = list[data_models.Choice]()
+                for choice_row in choices_rows:
+                    choices.append(data_models.Choice(
+                        option=choice_row.option,
+                        is_correct=choice_row.is_correct,
+                        explanation=choice_row.explanation
+                    ))
+                question = data_models.Question(
+                    question=question_row.question,
+                    choices=choices,
+                    study_recommendation=question_row.study_recommendation
+                )
+                pydantic_questions.append(question)
+            return pydantic_questions
+
         
